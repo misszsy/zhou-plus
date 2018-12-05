00ee2d0bc77c741129dc216c8cf8a1ed50b156a6
zhoushengyuan <zhoushengyuan@163.com>
2018-12-05 18:42:30 +0800

    first commit

--- zhou-admin/src/main/java/com/zhou/plus/zhouadmin/ZhouAdminApplication.java
+++ zhou-admin/src/main/java/com/zhou/plus/zhouadmin/ZhouAdminApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ZhouAdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZhouAdminApplication.class, args);
+		SpringApplication.run(
+		        ZhouAdminApplication.class, args
+        );
 	}
 }
