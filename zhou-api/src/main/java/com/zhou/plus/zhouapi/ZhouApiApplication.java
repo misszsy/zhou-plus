00598baea80c3423c10ab930b05258b1840dcb51
zhoushengyuan <zhoushengyuan@163.com>
2018-12-05 18:37:22 +0800

    first commit

---
+++ zhou-api/src/main/java/com/zhou/plus/zhouapi/ZhouApiApplication.java
@@ -0,0 +1,12 @@
+package com.zhou.plus.zhouapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhouApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhouApiApplication.class, args);
+	}
+}
