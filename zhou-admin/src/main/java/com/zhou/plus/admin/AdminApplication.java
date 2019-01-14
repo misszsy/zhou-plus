5f57a1880c39c8db9b40fbc38a50481d0955ecd1
zhoushengyuan_glb <zhoushengyuan_glb@qq.com>
2019-01-15 01:40:38 +0830

    first commit

--- zhou-admin/src/main/java/com/zhou/plus/admin/AdminApplication.java
+++ zhou-admin/src/main/java/com/zhou/plus/admin/AdminApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class AdminApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+
+        String root="https://blog.csdn.net/qq_36373262/article/details/60870832," +
+                "https://blog.csdn.net/zclcqlove/article/details/79962270";
         SpringApplication.run(AdminApplication.class, args);
     }
 }
