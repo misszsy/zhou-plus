fba5a4d7be43dd2bf1d139e332023b5085e6cea8
zhoushengyuan_glb <zhoushengyuan_glb@qq.com>
2019-03-06 01:52:15 +0830

    first commit

--- zhou-web/src/main/java/com/zhou/plus/web/controller/AboutController.java
+++ zhou-web/src/main/java/com/zhou/plus/web/controller/AboutController.java
@@ -3,20 +3,15 @@ package com.zhou.plus.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * 首页Controller
- * @author zhoushengyuan
- * @version 2019-01-27
- */
 @Controller
-public class IndexController {
+public class AboutController {
 
 
     /**
      * 前后端分离，跳转到前端的主页
      */
-    @GetMapping(value = {"/","index"})
+    @GetMapping(value = {"about"})
     public String index() {
-        return "index";
+        return "about/index";
     }
 }
